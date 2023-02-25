7ba900543eb2ae0f9a6313204c34386d716c482c
Tekktrik <tekktrik@gmail.com>
2023-02-25 14:02:36 -0500

    Update tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -4,8 +4,23 @@ import * as path from 'path'
 import * as cp from 'child_process'
 import * as process from 'process'
 import {test} from '@jest/globals'
+import * as fs from 'fs'
 
-test('Test runs', function () {
+const testif = function (cond: boolean) {
+  if (cond) {
+    return test
+  }
+  console.log(
+    'Test being skipped, use `npm run create-files` to create UF2 and filesystem files.'
+  )
+  return test.skip
+}
+
+const FIRMWARE_EXISTS = fs.existsSync('firmware.uf2')
+const FILESYSTEM_EXISTS = fs.existsSync('fat12.img')
+const FILES_EXIST = FIRMWARE_EXISTS && FILESYSTEM_EXISTS
+
+testif(FILES_EXIST)('Test runs', function () {
   process.env['INPUT_FIRMWARE'] = 'firmware.uf2'
   process.env['INPUT_FILESYSTEM'] = 'fat12.img'
   const np = process.execPath
@@ -13,6 +28,5 @@ test('Test runs', function () {
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
-  console.log('Running action...')
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
